2f3be0373c8d51d529462d86127d0526a4c15c2c
Tristan Lavocat <107067429+Tritounet1@users.noreply.github.com>
2025-07-27 21:03:22 +0200

    Fix les routes login et register

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,27 @@
-import client from "./client"
-import express from 'express'
+import client from "./client";
+import express from "express";
 import { createUser, getUser } from "./user";
 
 async function login(req: any, res: any) {
+  if (!req.body) {
+    res.send("Route excepted email and password.");
+    return;
+  }
+
   const email = req.body.email;
   const password = req.body.password;
-  
-  if(!email || !password) {
+
+  if (!email || !password) {
     res.send("Route excepted email and password.");
     return;
   }
 
   const token = await getUser(email, password);
+
+  if (!token) {
+    res.send("Invalid email or password.");
+    return;
+  }
 
   res.send({
     token: token,
@@ -19,10 +29,15 @@ async function login(req: any, res: any) {
 }
 
 async function register(req: any, res: any) {
+  if (!req.body) {
+    res.send("Route excepted email and password.");
+    return;
+  }
+
   const email = req.body.email;
   const password = req.body.password;
-  
-  if(!email || !password) {
+
+  if (!email || !password) {
     res.send("Route excepted email and password.");
     return;
   }
@@ -46,19 +61,19 @@ app.listen(port, () => {
   console.log(`App listening on port ${port}!`);
 });
 
-process.on('SIGINT', async () => {
-  console.log('Stopping server...');
+process.on("SIGINT", async () => {
+  console.log("Stopping server...");
   await client.$disconnect();
   process.exit(0);
 });
 
-process.on('SIGTERM', async () => {
-  console.log('Stopping server...');
+process.on("SIGTERM", async () => {
+  console.log("Stopping server...");
   await client.$disconnect();
   process.exit(0);
 });
 
-process.on('unhandledRejection', async (reason, promise) => {
+process.on("unhandledRejection", async (reason, promise) => {
   await client.$disconnect();
   process.exit(1);
 });
